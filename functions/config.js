eac70603e84a86bfd17fd4ca58eb60ddb3b14169
Claudio Schmidt Dev <blit.dev.br@gmail.com>
2026-04-24 13:52:40 -0300

    Commit de limpeza do Repositório

--- functions/config.js
+++
@@ -1,7 +0,0 @@
-// Configurações do Firebase para Cloud Functions
-// A API V1 usa OAuth 2.0 - não precisa de server key manual
-
-module.exports = {
-  PROJECT_ID: 'app-jeci-vieira-nails',
-  DATABASE_URL: 'app-jeci-vieira-nails-default-rtdb.firebaseio.com'
-};
